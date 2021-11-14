dab683334f5db630bf78d02d27705d5265810659
Michael Ehrlichman <ehrlichman.m@gmail.com>
2021-11-13 22:08:30 -0800

    Updates to handle 0 and 1 correctly.  Also get last digit via mod 10

---
+++ Fibonacci/FibMineLastDigit.py
@@ -0,0 +1,45 @@
+import sys
+import time
+
+def naive_FibRecurs(n):
+	if n <= 1:
+		return n
+	else:
+		return naive_FibRecurs(n-1) + naive_FibRecurs(n-2)
+
+def fast_Fib(n):
+	fib_lst = [0,1]
+	for i in range(2,n+1):
+		fib_lst.append(fib_lst[i-2]+fib_lst[i-1])
+	return fib_lst[-1]
+
+def faster_Fib(n):
+	fibA = 0
+	fibB = 1
+	for i in range(2,n+1):
+		fibC = (fibA + fibB)%10
+		fibA = fibB
+		fibB = fibC
+	if n==0:
+		return 0
+	elif n==1:
+		return 1
+	else:
+		return fibC
+
+#num = int(sys.argv[1])
+num = int(input())
+# t = time.process_time()
+# ans = naive_FibRecurs(num)
+# print(time.process_time()-t)
+# print(ans)
+
+#t = time.process_time()
+#ans = fast_Fib(num)
+#print(time.process_time()-t)
+#print(ans)
+
+#t = time.process_time()
+ans = faster_Fib(num)
+#print(time.process_time()-t)
+print(ans)
